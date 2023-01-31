bbfd8d6b305233a8e31d6f467d8a7759abf603d9
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-01-31 16:59:55 -0500

    Update mongodb connect

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const URI = "mongodb://127.0.0.1:27017/Contact-list-t";
+const URI = "mongodb://mongo:pRBWph8iCGpgCcuHGr78@containers-us-west-144.railway.app:7345";
 
 mongoose.set("strictQuery", true);
 mongoose
