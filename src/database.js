9a14b95d8861276e3646068649a51260aabc7daa
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-01-31 14:27:59 -0500

    database to deploy

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const URI = "mongodb://127.0.0.1:27017/Contact-list-t";
+const URI = "mongodb://localhost:3000/Contact-list-t";
 
 mongoose.set("strictQuery", true);
 mongoose
